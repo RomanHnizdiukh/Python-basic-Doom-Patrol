ae2ed2a5536c6039a83432a67c8d2cf09f53dd58
Roman Hnizdiukh <roman.hnz@gmail.com>
2022-01-13 23:12:00 +0100

    Adding 8 homework

--- homeworks/work_with_file/CalculatorApp/main.py
+++ homeworks/work_with_file/CalculatorApp/main.py
@@ -1,19 +1,31 @@
 while True:
     print("Welcome to calculator, please provide your operation with numbers: ")
 
-    num1 = int(input(" Enter the first number: "))
-    operator = input(" Enter operator:")
-    num2 = int(input(" Enter the second number: "))
-    result = num1 + num2
-    result2 = num1 - num2
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
 
-    if operator == "+":
-        print(f"{num1} {operator} {num2} = {result}")
+    print("Operation: +, -, *, /")
+    select = input("Select operations: ")
+
+    if select == "+":
+        print(num1, "+", num2, "=", num1 + num2)
         with open('result.txt', 'a') as file:
-            file.write(str(f"\n {num1} {operator} {num2} = {result} "))
-    elif operator == "-":
-        print(f"{num1} {operator} {num2} = {result2}")
+            file.write(str(f"\n {num1} {select} {num2} = {num1 + num2} "))
+
+    elif select == "-":
+        print(num1, "-", num2, "=", num1 - num2)
         with open('result.txt', 'a') as file:
-            file.write(str(f"\n {num1} {operator} {num2} = {result2} "))
+            file.write(str(f"\n {num1} {select} {num2} = {num1 - num2} "))
+
+    elif select == "*":
+        print(num1, "*", num2, "=", num1 * num2)
+        with open('result.txt', 'a') as file:
+            file.write(str(f"\n {num1} {select} {num2} = {num1 * num2} "))
+
+    elif select == "/":
+        print(num1, "/", num2, "=", num1 / num2)
+        with open('result.txt', 'a') as file:
+            file.write(str(f"\n {num1} {select} {num2} = {num1 / num2} "))
+
     else:
-        print("Invalid input, please provide operator: '+' or '-'")
+        print("Invalid input")
